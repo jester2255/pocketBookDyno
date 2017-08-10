9a5443336d485da4647846c07ad9fc7c68d89a61
jester2255 <jester2255pdunn@yahoo.com>
2017-08-09 21:01:51 -0600

    some math

--- assets/logic/muscle.js
+++ assets/logic/muscle.js
@@ -1,1 +1,18 @@
+var PI = 3.1415927
+
+var test = 5
+var test1
+var test2
 var thePower = 0;
+var tireDiam1 = 29;
+var tireDiam2 = 32;
+var speedo = 50;
+var actualSpeed ='';
+
+
+actualSpeed = (tireDiam2/tireDiam1)*speedo
+console.log(actualSpeed);
+test1 = Math.pow(test, 3)
+console.log(test1);
+test2 = Math.pow(test, PI)
+console.log(test2)
